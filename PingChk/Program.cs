1d67ac309cf5562ff431998138e70e825c0fe2d0
Gunjarit Yoothongin <n.me.dev@outlook.com>
2024-08-04 07:25:53 +0700

    Rename main UI

--- PingChk/Program.cs
+++ PingChk/Program.cs
@@ -16,7 +16,7 @@ namespace PingChk
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
