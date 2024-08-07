7d4b48c6ed537e26b3070fc0e4aed259553d1084
Gunjarit Yoothongin <n.me.dev@outlook.com>
2024-08-07 10:08:51 +0700

    Implement timer to Main form

--- PingChk/Main.cs
+++ PingChk/Main.cs
@@ -16,5 +16,41 @@ namespace PingChk
         {
             InitializeComponent();
         }
+
+        private async void tmrLatencyChecker_Tick(object sender, EventArgs e)
+        {
+            // Request for latency data
+            var latency = await Latency.GetServerLatencyAsync(txtServer.Text);
+            // Just push data into bar controller
+            bcMain.PushData(Convert.ToInt32(latency));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Define tag values
+            var clickToStartTag = "to:start";
+            var clickToStopTag = "to:stop";
+            var clickToStartText = "Start";
+            var clickToStopText = "Stop";
+            // Tag check
+            if (btnStart.Tag.ToString() == clickToStartTag)
+            {
+                // Then start the timer
+                tmrLatencyChecker.Start();
+                // Update button's tag
+                btnStart.Tag = clickToStopTag;
+                // Update button's text
+                btnStart.Text = clickToStopText;
+            }
+            else
+            {
+                // Then stop the timer
+                tmrLatencyChecker.Stop();
+                // Update button's tag
+                btnStart.Tag = clickToStartTag;
+                // Update button's text
+                btnStart.Text = clickToStartText;
+            }
+        }
     }
 }
