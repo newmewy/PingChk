268ad684d98dff591fcfa88b5354c589e47c6cbd
Gunjarit Yoothongin <n.me.dev@outlook.com>
2024-08-07 10:38:38 +0700

    Remove duplicate method

--- PingChk/Latency.cs
+++ PingChk/Latency.cs
@@ -65,33 +65,5 @@ namespace PingChk
                 }
             }
         }
-
-        public static long GetServerLatency(string serverAddress, int timeout = 500, int fallbackLatency = 500)
-        {
-            using (var ping = new Ping())
-            {
-                try
-                {
-                    // Send ping to specific server
-                    var reply = ping.Send(serverAddress, timeout);
-                    // If there is success ...
-                    if (reply.Status == IPStatus.Success)
-                    {
-                        // Return latency value in milliseconds
-                        return reply.RoundtripTime;
-                    }
-                    else
-                    {
-                        // Otherwise, return fallback value instead
-                        return fallbackLatency;
-                    }
-                }
-                catch
-                {
-                    // If there is error, return fallback as well
-                    return fallbackLatency;
-                }
-            }
-        }
     }
 }
