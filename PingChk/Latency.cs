b48eb9d178019564b156b353ff0727610a2ea674
Gunjarit Yoothongin <n.me.dev@outlook.com>
2024-08-07 09:25:28 +0700

    Update Latency class - Convert return type from double to long - Add non-async method for GetServerLatency

--- PingChk/Latency.cs
+++ PingChk/Latency.cs
@@ -9,7 +9,7 @@ namespace PingChk
 {
     public static class Latency
     {
-        public static async Task<double> GetServerLatencyAsync(string serverAddress, int timeout = 500, int fallbackLatency = 500)
+        public static async Task<long> GetServerLatencyAsync(string serverAddress, int timeout = 500, int fallbackLatency = 500)
         {
             using (var ping = new Ping())
             {
@@ -17,6 +17,34 @@ namespace PingChk
                 {
                     // Send ping to specific server
                     var reply = await ping.SendPingAsync(serverAddress, timeout);
+                    // If there is success ...
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        // Return latency value in milliseconds
+                        return reply.RoundtripTime;
+                    }
+                    else
+                    {
+                        // Otherwise, return fallback value instead
+                        return fallbackLatency;
+                    }
+                }
+                catch
+                {
+                    // If there is error, return fallback as well
+                    return fallbackLatency;
+                }
+            }
+        }
+
+        public static long GetServerLatency(string serverAddress, int timeout = 500, int fallbackLatency = 500)
+        {
+            using (var ping = new Ping())
+            {
+                try
+                {
+                    // Send ping to specific server
+                    var reply = ping.Send(serverAddress, timeout);
                     // If there is success ...
                     if (reply.Status == IPStatus.Success)
                     {
